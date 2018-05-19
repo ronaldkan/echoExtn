7ef91a7d6279de3492c6ba9b0dc6daafdbf49a41
Poh Peng <wangpp1@hotmail.com>
2018-05-19 23:53:10 +0800

    clean code

--- background.js
+++ background.js
@@ -31,37 +31,3 @@ chrome.runtime.onMessage.addListener(
     //   if (request.greeting == "hello")
         // sendResponse({farewell: "goodbye"});
     });
-
-function addHighlightableExpression(info, tab) {
-    console.log("info", info);
-    var contentObj = new Object();
-        contentObj.author = "username";
-        contentObj.address = info.pageUrl;
-        contentObj.detail = info.selectionText;
-
-        console.log("contentObj", contentObj);
-        $.post("http://40.74.71.24/content", contentObj, function(data, status) { 
-            console.log(data);
-        });
-
-    chrome.tabs.query({
-        "active": true,
-        "currentWindow": true
-    }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {
-            "wordToHighlight": info.selectionText
-        });
-    });
-}
-
-var contexts = ["selection"];
-
-for (var i = 0; i < contexts.length; i++) {
-    var context = contexts[i];
-    var title = "Highlight expression";
-    var id = chrome.contextMenus.create({
-        "title": title,
-        "contexts": [context],
-        "onclick": addHighlightableExpression
-    });
-}
--- contentScript.js
+++ contentScript.js
@@ -26,191 +26,73 @@ function toggleScreen() {
     return false;
 }
 
-var LAST_SELECTION,
-    LAST_ELEMENT;
+// function applyReplacementRule(node) {
+//     // Ignore any node whose tag is banned
+//     if (!node || $.inArray(node.tagName, hwBannedTags) !== -1) { return; }
 
-function applyReplacementRule(node) {
-    // Ignore any node whose tag is banned
-    if (!node || $.inArray(node.tagName, hwBannedTags) !== -1) { return; }
+//     try {
+//         $(node).contents().each(function (i, v) {
+//             // Ignore any child node that has been replaced already or doesn't contain text
+//             if (v.isReplaced || v.nodeType !== Node.TEXT_NODE) { return; }
 
-    try {
-        $(node).contents().each(function (i, v) {
-            // Ignore any child node that has been replaced already or doesn't contain text
-            if (v.isReplaced || v.nodeType !== Node.TEXT_NODE) { return; }
+//             // Apply each replacement in order
+//             hwReplacements.then(function (replacements) {
+//                 if (replacements.words) {
+//                     replacements.words.forEach(function (replacement) {
+//                         //if( !replacement.active ) return;
+//                         var matchedText = v.textContent.match(new RegExp(replacement, "i"));
 
-            // Apply each replacement in order
-            hwReplacements.then(function (replacements) {
-                if (replacements.words) {
-                    replacements.words.forEach(function (replacement) {
-                        //if( !replacement.active ) return;
-                        var matchedText = v.textContent.match(new RegExp(replacement, "i"));
+//                         if (matchedText) {
+//                             // Use `` instead of '' or "" if you want to use ${variable} inside a string
+//                             // For more information visit https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+//                             highlightColor.then(function (item) {
+//                                 var color = (item.color.startsWith("#")) ? item.color : "#" + item.color;
+//                                 var replacedText = node.innerHTML.replace(new RegExp(`(${replacement})`, "i"), `<span style="background-color: ${color}">$1</span>`);
 
-                        if (matchedText) {
-                            // Use `` instead of '' or "" if you want to use ${variable} inside a string
-                            // For more information visit https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-                            highlightColor.then(function (item) {
-                                var color = (item.color.startsWith("#")) ? item.color : "#" + item.color;
-                                var replacedText = node.innerHTML.replace(new RegExp(`(${replacement})`, "i"), `<span style="background-color: ${color}">$1</span>`);
+//                                 node.innerHTML = replacedText;
+//                             });
+//                         }
+//                     });
+//                 }
+//             }).catch(function (reason) {
+//                 console.log("Handle rejected promise (" + reason + ") here.");
+//             });
 
-                                node.innerHTML = replacedText;
-                            });
-                        }
-                    });
-                }
-            }).catch(function (reason) {
-                console.log("Handle rejected promise (" + reason + ") here.");
-            });
+//             v.isReplaced = true;
+//         });
+//     } catch (err) {
+//         // Basically this means that an iframe had a cross-domain source
+//         if (err.name !== "SecurityError") { throw err; }
+//     }
+// }
 
-            v.isReplaced = true;
-        });
-    } catch (err) {
-        // Basically this means that an iframe had a cross-domain source
-        if (err.name !== "SecurityError") { throw err; }
-    }
-}
+// hwReplacements = new Promise(function (resolve, reject) {
 
-function storeWords(wordList) {
-    chrome.storage.local.set({ "words": wordList }, function () {
-        // autoReload.then(function (value) {
-        // if(value.autoReload) {
-        //     window.location.reload();
-        // }
-        // });
-    });
-}
+//     chrome.runtime.sendMessage({loadHighlights: true, url : "http://40.74.71.24/content", link: window.location.href}, function(response) {
+//         chrome.storage.local.set({"words" : response.data}, function(result) {
 
-function storeColor(hexCode) {
-    chrome.storage.local.set({ "color": hexCode }, function () { });
-}
+//         });
+//         chrome.storage.local.get("words", function (items) {
+//             resolve(items);
+//         });
+//     });
 
-hwReplacements = new Promise(function (resolve, reject) {
+// });
 
-    chrome.runtime.sendMessage({loadHighlights: true, url : "http://40.74.71.24/content", link: window.location.href}, function(response) {
-        chrome.storage.local.set({"words" : response.data}, function(result) {
-
-        });
-        chrome.storage.local.get("words", function (items) {
-            resolve(items);
-        });
-    });
-
-});
-
-highlightColor = new Promise(function (resolve, reject) {
-    chrome.storage.local.get("color", function (item) {
-        resolve(item);
-    });
-});
-
-// autoReload = new Promise(function (resolve, reject) {
-//     chrome.storage.local.get("autoReload", function (items) {
-//         resolve(items);
+// highlightColor = new Promise(function (resolve, reject) {
+//     chrome.storage.local.get("color", function (item) {
+//         resolve(item);
 //     });
 // });
 
-function getWordList() {
-    var words = [];
-
-    $(".wordList li").each(function (index, element) {
-        words.push(element.textContent.trim());
-    });
-
-    return words;
-}
-
-chrome.extension.onMessage.addListener(function (message, sender, callback) {
-    
-    // On message receive from background js of highlighting text
-    console.log(LAST_SELECTION);
-    var selectedText = LAST_SELECTION.extractContents();
-    var span= document.createElement("span");
-    span.style.backgroundColor = "#"+finalColor;
-    span.appendChild(selectedText);
-    LAST_SELECTION.insertNode(span);
-
-    if (message.wordToHighlight) {
-
-        // var contentObj = new Object();
-        // contentObj.author = "username";
-        // contentObj.address = "aaa";
-        // contentObj.detail = message.wordToHighlight;
-
-        // console.log("contentObj", contentObj);
-        // $.post("http://40.74.71.24/content", contentObj, function(data, status) { 
-        //     alert('posted');
-        //     console.log(data);
-        // });
-
-        hwReplacements.then(function (wordList) {
-            if (wordList.words) {
-                wordList.words.push(message.wordToHighlight);
-                storeWords(wordList.words);
-            } else {
-                var words = [message.wordToHighlight];
-                storeWords(words);
-            }
-        });
-    }
-    
-});
-
 $(function () {
     $("body *").map(function (i, v) { applyReplacementRule(v); });
-
-    hwReplacements.then(function (replacements) {
-        if (replacements.words) {
-            replacements.words.forEach(function (replacement, index) {
-                $(".wordList").append(`<li>${replacement} <i class="fa fa-trash right" aria-hidden="true"></i></li>`);
-            });
-        }
-    }).catch(function (reason) {
-        console.log("Handle rejected promise (" + reason + ") here.");
-    });
-
-    highlightColor.then(function (item) {
-        if(item.color) {
-            finalColor = item.color;
-            $(".jscolor").val(item.color);
-            var color = (item.color.startsWith("#")) ? item.color : "#" + item.color;
-            $(".highlight").css("background-color", color);
-        }
-    });
-
-    // autoReload.then(function (value) {
-    //     if(value.autoReload) {
-    //         $("#autoReloadCheck").prop("checked", true);
-    //     } else {
-    //         $("#autoReloadCheck").prop("checked", false);
-    //     }
-    // });
-
-    $(document).on("click", ".fa-trash", function () {
-        $(this).parent().remove();
-        storeWords(getWordList());
-    });
-
-    $(".jscolor").change(function (e) {
-        storeColor($(".jscolor").val());
-    });
-
-    // $("#autoReloadCheck").change(function (e) {
-    //     var checked;
-    //     if($(this).is(":checked")) {
-    //         checked = true;
-    //     } else {
-    //         checked = false;
-    //     }
-
-    //     chrome.storage.local.set({ "autoReload": checked }, function () { });
-    // });
 });
 
 // Add bubble to the top of the page.
 // var bubbleDOM = document.createElement('div');
 // bubbleDOM.setAttribute('class', 'selection_bubble');
 // document.body.appendChild(bubbleDOM);
-
-
 
 function replaceSelectionWithHtml(html) {
     var range;
@@ -283,10 +165,3 @@ chrome.extension.onMessage.addListener(function (request, sender, sendResponse) 
     if (request.method == "getSelection")
         console.log(window.getSelection().toString());
 });
-
-
-document.body.addEventListener('contextmenu', function(e) {
-    LAST_SELECTION = window.getSelection().getRangeAt(0);
-    LAST_ELEMENT = e.target;
-    // this will update your last element every time you right click on some element in the page
-}, false);
