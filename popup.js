04910c79dee6a166cf0dab583b576145b879af2c
Poh Peng <wangpp1@hotmail.com>
2018-05-19 13:43:38 +0800

    added fake login

--- popup.js
+++ popup.js
@@ -1,5 +1,29 @@
 
 document.addEventListener('DOMContentLoaded', function() {
+
+    
+    chrome.storage.local.get("uname", function (uname) {
+        console.log('name',uname);
+        if (Object.keys(uname).length === 0){
+            hideSignOut(); 
+        }else {
+            hideSignIn();
+        }
+    });
+
+    var signInButton = document.getElementById('signIn');
+    signInButton.addEventListener('click', function() {
+        chrome.storage.local.set({ "uname": "random" }, function () { });
+        window.location.reload();
+    });
+
+    var signOutButton = document.getElementById('signOut');
+    signOutButton.addEventListener('click', function() {
+        chrome.storage.local.remove("uname", function () {
+            window.location.reload();
+         });
+    });
+
     var checkPageButton = document.getElementById('checkPage');
     checkPageButton.addEventListener('click', function() {
         console.log('clicked');
@@ -20,3 +44,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
 console.log('page on load test');
 var socket = io('http://localhost:3001');
+
+function hideSignIn()  
+{  
+   document.getElementById("signIn").style.display="none";  
+}
+
+function hideSignOut()  
+{  
+   document.getElementById("signOut").style.display="none";  
+}
